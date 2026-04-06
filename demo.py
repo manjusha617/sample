f844799368a5454197f41035b9e2482fb8c7f5b6
manjusha617 <manjupothabathini@gmail.com>
2026-04-06 15:00:10 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,17 @@
-position=input("enter position:")
-column=position[0]
-row=int(position[1])
-column_num=ord(column)-ord('a')+1
-total=column_num+row
-if total % 2 ==0:
-  print("black")
-else:
-  print("white")
+import streamlit as st
+
+st.title("Chess Board Color Finder ♟️")
+
+position = st.text_input("Enter position (like a1, b3):")
+
+if position:
+    column = position[0].lower()
+    row = int(position[1])
+
+    column_num = ord(column) - ord('a') + 1
+    total = column_num + row
+
+    if total % 2 == 0:
+        st.write("Black ⬛")
+    else:
+        st.write("White ⬜")
