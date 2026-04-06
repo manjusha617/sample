56dd9475287f00f57441e2e37e073e7bf92bd66b
manjusha617 <manjupothabathini@gmail.com>
2026-04-06 14:54:02 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
-import streamlit as st
-st.title("welcome to streamlit!")
+position=input("enter position:")
+column=position[0]
+row=int(position[1])
+column_num=ord(column)-ord('a')+1
+total=column_num+row
+if total % 2 ==0:
+  print("black")
+else:
+  print("white")
