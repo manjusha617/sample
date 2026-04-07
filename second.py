ef4b423f230948a3df3b06f22f53793837745fab
manjusha617 <manjupothabathini@gmail.com>
2026-04-07 15:56:07 +0530

    Update second.py

--- second.py
+++ second.py
@@ -1,21 +1,22 @@
 import streamlit as st
 st.title("speed fine")
 speed=st.number_input("enter the speed ravi travelled per hour:")
-helmet=st.selectbox("enter is wearing helmet:(yes/no):")
-documents=st.selectbox("enter is he carriying:(yes/no):")
+helmet=st.selectbox("enter is wearing helmet?",["yes","no"])
+documents=st.selectbox("enter is he carriying?",["yes","no"])
 if st.button("calculate bill"):
     total=0
-if speed>80 and helmet=="no" and documents =="no":
-    total=2500
-else:
-  total=0
-  if helmet == "no":
-     total+=500
-  if documents=="no":
+    if speed>80 and helmet=="no" and documents =="no":
+        total=2500
+    else:
+      total=0
+    if helmet == "no":
+      total+=500
+    if documents=="no":
      total+=700
-  if speed>80:
-    total+=1000
-if total>0:
-  st.error("total fine:",total)
-else:
-  st.sucess("he didn't have to pay any fine.")
+    if speed>80:
+     total+=1000
+    if total>0:
+       st.error("total fine:",total)
+    else:
+       st.sucess("he didn't have to pay any fine.")
+
